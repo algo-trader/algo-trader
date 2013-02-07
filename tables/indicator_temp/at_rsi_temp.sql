abde03992c50820630984e0bd993a03c8495f6cd
Dacheng Zhu <dacheng.zhu@gmail.com>
2013-02-08 04:06:38 +0800

    2

---
+++ tables/indicator_temp/at_rsi_temp.sql
@@ -0,0 +1,14 @@
+/* Formatted on 2013/02/08 4:04:34 AM (QP5 v5.126.903.23003) */
+CREATE TABLE at_rsi_temp
+(
+   ticker   VARCHAR2 (100 BYTE)
+ , day      DATE
+ , close    NUMBER
+ , u        NUMBER
+ , d        NUMBER
+ , n        INTEGER
+ , ema_u    NUMBER
+ , ema_d    NUMBER
+ , rs       NUMBER
+ , rsi      NUMBER
+);
